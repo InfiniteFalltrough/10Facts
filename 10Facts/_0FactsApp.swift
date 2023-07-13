59cafe910b4c5e72acfa600d6d5a340ccefe6410
Viktor Golubenkov <vityandra@Viktors-MacBook-Pro.local>
2023-07-14 01:43:12 +0700

    initial

---
+++ 10Facts/_0FactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0FactsApp.swift
+//  10Facts
+//
+//  Created by Viktor Golubenkov on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct _0FactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
