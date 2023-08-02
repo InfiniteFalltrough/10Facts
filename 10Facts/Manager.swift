188ffba5a264d5de4770ace6da0c45a7a5e614c6
Viktor Golubenkov <vityandra@Viktors-MacBook-Pro.local>
2023-08-02 16:41:31 +0700

    Singleton + Observer

---
+++ 10Facts/Manager.swift
@@ -0,0 +1,8 @@
+//
+//  Manager.swift
+//  10Facts
+//
+//  Created by Viktor Golubenkov on 7/15/23.
+//
+
+import Foundation
---
+++ 10Facts/StarModel.swift
@@ -0,0 +1,14 @@
+//
+//  DataModel.swift
+//  10Facts
+//
+//  Created by Viktor Golubenkov on 7/15/23.
+//
+
+import Foundation
+
+struct StarModel: Codable {
+    let title: Star
+    let facts: String
+    let imageURL: String
+}
